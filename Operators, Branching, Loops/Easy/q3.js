9befb5947a7318c26c0178cdb2d3f057257d86af
harshil114 <harshilvispute114@gmail.com>
2024-05-20 16:23:03 +0530

    solution for q3

---
+++ Operators, Branching, Loops/Easy/q3.js
@@ -0,0 +1,11 @@
+function minMax(num1,num2){
+    console.log("Num1 : "+ num1, "Num2 : "+num2);
+    if(num1>num2){
+        console.log("Num 1 is maximum");
+        console.log("Num 2 is minimum");
+    }else{
+        console.log("num 2 is maximum");
+        console.log("num 1 is minimum");
+    }
+}
+minMax(129,251);
