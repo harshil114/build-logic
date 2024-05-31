dbe08ed56a957d8bc84a8697fbf3b635cf636609
harshil114 <harshilvispute114@gmail.com>
2024-05-31 16:31:23 +0530

    solve q5

---
+++ Numbers/q5.js
@@ -0,0 +1,19 @@
+let a = 3
+function area(a){
+    return a*a;
+}
+function perimeter(a){
+    return 4*a;
+}
+function surfaceArea(a){
+    return 6*a*a;
+}
+function volume(a){
+    return a*a*a;
+}
+
+console.log(area(a));
+console.log(perimeter(a));
+console.log(surfaceArea(a));
+console.log(volume(a));
+
